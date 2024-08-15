40d08e32705dcef19c814990f5cd7c50358bdcc5
M@ <1641788+yoat@users.noreply.github.com>
2024-08-14 21:15:26 -0400

    Simple data load and palette test

--- src/Scene.tsx
+++ src/Scene.tsx
@@ -2,13 +2,28 @@ import { OrbitControls } from '@react-three/drei'
 import { useFrame } from '@react-three/fiber'
 import { useControls } from 'leva'
 import { Perf } from 'r3f-perf'
-import { useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { BoxGeometry, Mesh, MeshBasicMaterial } from 'three'
 import { Cube } from './components/Cube'
 import { Plane } from './components/Plane'
 import { Sphere } from './components/Sphere'
+import trilogyData from './data/trilogy.json';
+import colorPalette from './data/palette.json';
+
+export interface ITrilogy {
+  Trilogy: string;
+  AlphaScore: number;
+  AlphaYear: number;
+  BravoScore: number;
+  BravoYear: number;
+  CharlieScore: number;
+  CharlieYear: number;
+}
 
 function Scene() {
+  const [trilogies, setTrilogies] = useState<ITrilogy[]>(trilogyData);
+  const [palette, setPalette] = useState<string[]>(colorPalette);
+
   const { performance } = useControls('Monitoring', {
     performance: false,
   })
@@ -25,6 +40,10 @@ function Scene() {
     }
   })
 
+  useEffect(() => {
+    console.log('trilogies:', trilogies);
+  }, [trilogies]);
+
   return (
     <>
       {performance && <Perf position='top-left' />}
@@ -39,6 +58,15 @@ function Scene() {
       />
       <ambientLight intensity={0.2} />
 
+      { trilogies.map((trilogy: ITrilogy, idx: number) => {
+        return (
+          <mesh key={idx} position={[idx * 2, 0, 0]}>
+            <boxGeometry args={[1, 1, 1]} />
+            <meshBasicMaterial color={palette[idx]} />
+          </mesh>
+        )
+        })
+      }
       <Cube ref={cubeRef} />
       <Sphere />
       <Plane />
